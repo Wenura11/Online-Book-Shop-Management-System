0454218a24bd495aab0bde4dbafb95827fb13f15
Wenura_Gunasekara <wenura618@gmail.com>
2024-08-31 11:18:43 +0530

    add comment

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,6 +1,6 @@
 import React, { createContext, useEffect, useState } from "react";
-import {Routes, Route} from 'react-router-dom';
-import LoginPage  from "./pages/loginpage/LoginPage";
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/loginpage/LoginPage";
 import RegisterPage from "./pages/loginpage/RegisterPage";
 import HomePage from "./pages/homepage/HomePage";
 import BooksPage from "./pages/bookspage/BooksPage";
@@ -17,42 +17,40 @@ import AllBooksPage from "./pages/allbookspage/AllBooksPage";
 export const CartContext = createContext([]);
 
 const App = () => {
+  const [cartItem, setCartItem] = useState([]);
+  const [totalAmount, setTotalAmount] = useState(0);
 
-const [cartItem, setCartItem] = useState([])
-const [totalAmount,setTotalAmount] = useState(0);
+  useEffect(() => {
+    let total = 0;
+    cartItem.forEach((item) => {
+      total = total + parseInt(item.price);
+    });
 
-useEffect(() => {
-  let total = 0;
-  cartItem.forEach((item) => {
-    total = total + parseInt(item.price);
-  })
+    setTotalAmount(total);
+  }, [cartItem]);
 
-  setTotalAmount(total);
-},[cartItem])
-
-
-  return(
+  return (
     <ScrollToTop>
-      <CartContext.Provider value = {{cartItem ,totalAmount, setCartItem}}>
-      <Routes>
-        
-        <Route path="/" element={<LoginPage/>} /> 
-        <Route path="/Admin" element={<AdminPage/>} /> 
-        <Route path="/Addbooks" element={<AddBookPage/>} /> 
-        <Route path="/Updatebooks" element={<UpdateBookPage/>} /> 
-        <Route path="/Removebooks" element={<RemoveBookPage/>} /> 
-        <Route path="/BookList" element={<AllBooksPage/>} /> 
-        <Route path="/register" element={<RegisterPage/>} /> 
-        <Route path="/home" element={<HomePage/>} />
-        <Route path="/books" element={<BooksPage/>} />
-        <Route path="/cart" element={<CartPage/>}/>
-        <Route path="/search" element={<SearchPage/>} />
-        <Route path="/book-details/:id" element={<BookDetaisPage/>} />
-       
-      </Routes>
+      <CartContext.Provider value={{ cartItem, totalAmount, setCartItem }}>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/Admin" element={<AdminPage />} />
+          <Route path="/Addbooks" element={<AddBookPage />} />
+          <Route path="/Updatebooks" element={<UpdateBookPage />} />
+          <Route path="/Removebooks" element={<RemoveBookPage />} />
+          <Route path="/BookList" element={<AllBooksPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/books" element={<BooksPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/book-details/:id" element={<BookDetaisPage />} />
+        </Routes>
       </CartContext.Provider>
     </ScrollToTop>
-  )
-}
+  );
+};
 
 export default App;
+
+//add comment
